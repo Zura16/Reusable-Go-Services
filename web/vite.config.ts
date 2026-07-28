a22114f2aec4d226f1d448ff2f72a681f763e7ab
AalindCS <kaleaalind7@gmail.com>
2026-07-28 11:53:36 -0700

    ci(web): configure GitHub Pages deployment with gh-pages and automated GitHub Actions workflow

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Reusable-Go-Services/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
